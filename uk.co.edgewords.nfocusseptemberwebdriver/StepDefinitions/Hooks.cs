5e92aba682545711f3ce95a75813798219c0d831
Bibek Rai <bibek.rai@nfocus.co.uk>
2022-10-13 16:26:34 +0100

    changes

--- uk.co.edgewords.nfocusseptemberwebdriver/StepDefinitions/Hooks.cs
+++ uk.co.edgewords.nfocusseptemberwebdriver/StepDefinitions/Hooks.cs
@@ -34,6 +34,8 @@ namespace Practise.StepDefinitions
             s_driver.Manage().Window.Maximize();
             s_driver.FindElement(By.CssSelector(".woocommerce-store-notice__dismiss-link")).Click();
             _scenarioContext["myDriver"] = s_driver;
+
+            Console.Write("Logged in!");
         }
 
         [AfterScenario]
