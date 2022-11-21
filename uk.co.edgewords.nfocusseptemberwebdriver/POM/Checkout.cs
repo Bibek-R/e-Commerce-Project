d76236a7fbf9887de7fe105c3160b48c942b8e87
Bibek-R <113366199+Bibek-R@users.noreply.github.com>
2022-11-21 10:04:25 +0000

    Update Checkout.cs

--- uk.co.edgewords.nfocusseptemberwebdriver/POM/Checkout.cs
+++ uk.co.edgewords.nfocusseptemberwebdriver/POM/Checkout.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 
 namespace Practise.POM
@@ -21,50 +22,95 @@ namespace Practise.POM
 
         //Locators
 
-        IWebElement firstName => driver.FindElement(By.Name("billing_first_name"));
-        IWebElement lastName => driver.FindElement(By.Name("billing_last_name"));
-        IWebElement streetAddress => driver.FindElement(By.Name("billing_address_1"));
-        IWebElement town => driver.FindElement(By.Name("billing_city"));
-        IWebElement postcode => driver.FindElement(By.Name("billing_postcode"));
-        IWebElement phone => driver.FindElement(By.Name("billing_phone"));
-        IWebElement paymentMethod => driver.FindElement(By.CssSelector("li.wc_payment_method:nth-child(1)>label:nth-child(2)"));
-        IWebElement placeOrder => driver.FindElement(By.Name("woocommerce_checkout_place_order"));
+        IWebElement _setfirstName => driver.FindElement(By.Name("billing_first_name"));
+        IWebElement _setlastName => driver.FindElement(By.Name("billing_last_name"));
+        IWebElement _setstreetAddress => driver.FindElement(By.Name("billing_address_1"));
+        IWebElement _setTown => driver.FindElement(By.Name("billing_city"));
+        IWebElement _setPostcode => driver.FindElement(By.Name("billing_postcode"));
+        IWebElement _setPhone => driver.FindElement(By.Name("billing_phone"));
+        IWebElement paymentMethod => driver.FindElement(By.CssSelector(".payment_method_cheque.wc_payment_method > label"));
+        IWebElement placeOrder => driver.FindElement(By.Id("place_order"));
+        IWebElement _setEmail => driver.FindElement(By.Name("billing_email"));
+        IWebElement cashonDelivery => driver.FindElement(By.XPath("/html/body/div/div[2]/div/div[1]/main/article/div/div/form[2]/div[2]/div/ul/li[2]/label"));
 
 
         //Service Methods
 
-        public void FirstName()
+        public Checkout setfirstName(string value)  //Method to set FirstName
         {
-          firstName.Click();
+            _setfirstName.Clear();
+            _setfirstName.SendKeys(value);
+            return this;
+
         }
-        public void LastName()
+        public Checkout setlastName(string value)   //Method to set LastName
         {
-            lastName.Click();
+            _setlastName.Clear();
+            _setlastName.SendKeys(value);
+            return this;
+
         }
-        public void StreetAddress()
+        public Checkout setstreetAddress(string value)   //Method to set Address
         {
-            streetAddress.Click();
+            _setstreetAddress.Clear();
+            _setstreetAddress.SendKeys(value);
+            return this;
+
         }
-        public void Town()
+        public Checkout setTown(string value)      //Method to set Town
         {
-            town.Click();
+            _setTown.Clear();
+            _setTown.SendKeys(value);
+            return this;
+
         }
-        public void Postcode()
+        public Checkout setPostcode(string value)    //Method to set Postcode
         {
-            postcode.Click();
+            _setPostcode.Clear();
+            _setPostcode.SendKeys(value);
+            return this;
+
         }
-        public void Phone()
+        public Checkout setPhone(string value)     //Method to set Phone number
         {
-            phone.Click();
+            _setPhone.Clear();
+            _setPhone.SendKeys(value);
+            return this;
+
         }
-        public void PaymentMethod()
+        public Checkout setEmail(string value)    //Method to set the email
+        {
+            _setEmail.Clear();
+            _setEmail.SendKeys(value);
+            return this;
+
+        }
+
+        //method to get checkout information
+        public void details(string firstname, string lastname, string address, string town, string postcode, string phone, string email)
+        {
+            setfirstName(firstname);
+            setlastName(lastname);
+            setstreetAddress(address);
+            setTown(town);
+            setPostcode(postcode);
+            setPhone(phone);
+            setEmail(email);
+        }    
+        
+
+        public void PaymentMethod()      //method to select the payment method
         {
             paymentMethod.Click();
         }
-        public void PlaceOrder()
+        public void PlaceOrder()    //method to place order
         {
             placeOrder.Click();
         }
 
+        public void CashOnDelivery()
+        {
+            cashonDelivery.Click();
+        }
     }
 }
